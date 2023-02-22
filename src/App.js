a7e2c3ec9ef7721444705f3ea76df1f1db2e6121
김민정 <mjkim@gaia3d.com>
2023-02-22 14:39:30 +0900

    component 만들기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+function Food ({name}) {
+  return (
+    <h3>i love {name}</h3>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       Hello!
+      <Food name="kimchi" />
+      <Food name="potato"/>
     </div>
   );
 }
