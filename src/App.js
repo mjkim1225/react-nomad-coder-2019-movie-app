2f6e997cff84a7c21382ab21afcd6c1da59c5362
김민정 <mjkim@gaia3d.com>
2023-02-22 15:23:08 +0900

    feat: food component 사용 X

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
 import React from "react";
-import Food from "./Food.js";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "bulgogi",
-    image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
-    rating: 5
-  },
-  {
-    id: 3,
-    name: "kimbap",
-    image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
-    rating: 5
-  },
-  {
-    id: 4,
-    name: "samgyetang",
-    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
-    rating: 5
-  }
-]
-
-function renderFood(f) {
-  return (<Food key={f.id} name={f.name} pic={f.image} rating={f.rating}></Food>);
-}
 
 function App() {
   return (
     <div className="App">
       Hello!
-      { foodILike.map(renderFood)}
     </div>
   );
 }
