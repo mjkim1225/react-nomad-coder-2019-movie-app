615d608e15828d8e600b712775755f97a2b81c33
김민정 <mjkim@gaia3d.com>
2023-02-22 16:18:52 +0900

    feat: 다시 App.js init

--- src/App.js
+++ src/App.js
@@ -2,21 +2,9 @@ import React from "react";
 
 
 class App extends React.Component {
-    state = {
-        count: 0
-    };
-    add = () => {
-        this.setState(current => ( { count: current.count+1 } ) );
-    };
-    minus = () => {
-        this.setState(current => ( { count: current.count-1 } ) );
-    };
     render() {
         return(
             <div>
-                <h1>I am a class comp. {this.state.count} </h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
             </div>
         );
     }
