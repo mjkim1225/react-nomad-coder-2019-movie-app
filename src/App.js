c4bc078f56ff84183d57b6b72f61efc02f379233
김민정 <mjkim@gaia3d.com>
2023-02-22 14:53:22 +0900

    feat: vscode -> intellij

--- src/App.js
+++ src/App.js
@@ -2,32 +2,36 @@ function Food ({name, pic}) {
   return (
     <div>
         <h3>i love {name}</h3>
-        <img src={pic} />
+        <img src={pic} alt={name} />
     </div>
   )
 }
 
 const foodILike = [
   {
+  id: 1, 
   name: "Kimchi",
   image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
   },
   {
+  id: 2, 
   name: "bulgogi",
   image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM"
   },
   {
+  id: 3, 
   name: "kimbap",
   image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM"
   },
   {
+  id: 4, 
   name: "samgyetang",
   image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
   }
 ]
 
 function renderFood(f) {
-  return (<Food name={f.name} pic={f.image} ></Food>);
+  return (<Food key={f.id} name={f.name} pic={f.image} ></Food>);
 }
 
 function App() {
