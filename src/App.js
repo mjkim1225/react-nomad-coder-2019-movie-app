e34562897f010782276732c158b812fb86251281
김민정 <mjkim@gaia3d.com>
2023-02-22 15:47:54 +0900

    feat: state, set state 사용법

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 
 
-function App() {
-  return (
-    <div className="App">
-      Hello!
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        count: 0
+    };
+    add = () => {
+        this.setState(current => ( { count: current.count+1 } ) );
+    };
+    minus = () => {
+        this.setState(current => ( { count: current.count-1 } ) );
+    };
+    render() {
+        return(
+            <div>
+                <h1>I am a class comp. {this.state.count} </h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
+    }
 }
 
 export default App;
