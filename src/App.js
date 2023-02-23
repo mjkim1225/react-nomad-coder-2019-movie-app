10b0ee6f8ee92c82d8265e30dea86ee66a86a247
김민정 <mjkim@gaia3d.com>
2023-02-23 09:36:25 +0900

    feat: router 시작하기
    npm i react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,49 +1,7 @@
-import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
+import React from "react"
 
-class App extends React.Component {
-    state = {
-        isLoading: true,
-        movies: [],
-    }
-    getMovies = async () => {
-        const {data: {data: {movies}} }= await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-        console.log(movies);
-        this.setState( { isLoading: false } );
-        this.setState( { movies } );
-    }
-    componentDidMount() {
-        this.getMovies();
-    }
-
-    render() {
-        const {isLoading, movies} = this.state;
-        return(
-            <section className="container">
-                {isLoading ?
-                    (<div className="loader">
-                        <span className="loader_txt"> Loading... </span>
-                    </div>)
-                    :
-                   ( <div className="movies">
-                       {movies.map(movie => {
-                            return (<Movie
-                            key={movie.id}
-                            id={movie.id}
-                            year={movie.year}
-                            title={movie.title}
-                            summary={movie.summary}
-                            poster={movie.poster}
-                            genres={movie.genres}
-                            />)
-                        } )}
-                    </div>)
-                }
-
-            </section>
-        );
-    }
+function App() {
+    return <span> something </span>
 }
 
 export default App;
