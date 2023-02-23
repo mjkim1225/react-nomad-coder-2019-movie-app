eee57a07248fbc4f7845cf0a8be27b6ee1a1dcea
김민정 <mjkim@gaia3d.com>
2023-02-23 10:10:44 +0900

    feat: router와 navigation 만듦

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from './route/About';
+import Home from "./route/Home";
+import Navigation from "./component/Navigation";
 
 function App() {
-    return <span> something </span>
+    return (
+    <BrowserRouter>
+        <Navigation />
+        <Routes>
+            <Route path="/" element={<Home />} > </Route>
+            <Route path="/about" element={<About />} > </Route>
+        </Routes>
+    </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ src/route/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function About(props) {
+    console.log(props);
+    return (
+        <span>About this page: I built it </span>
+    )
+}
+
+export default About;
